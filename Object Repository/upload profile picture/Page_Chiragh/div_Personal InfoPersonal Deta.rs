9b83be45981a38d48051528692a0821b82edd4c9
HP <HP@DESKTOP-2D7LI1O>
2019-01-22 13:30:14 +0500

    upload profile picture testcase.

---
+++ Object Repository/upload profile picture/Page_Chiragh/div_Personal InfoPersonal Deta.rs
@@ -0,0 +1,71 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Personal InfoPersonal Deta</name>
+   <tag></tag>
+   <elementGuidId>58bdae9a-9636-45b6-890f-6a36a3b95718</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='account']/div/div/div</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;account&quot;]/div/div[1]/div/div/input</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>ud-sidebar</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Personal InfoPersonal DetailsContact DetailsChange Password</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;account&quot;]/div/div[1]/div/div/input</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <value>//div[@id='account']/div/div/div</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Activity'])[1]/following::div[5]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Account'])[1]/following::div[5]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <value>//*[@id=&quot;account&quot;]/div/div[1]/div/div/img</value>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Scripts/upload profile picture/Script1548145490140.groovy
@@ -0,0 +1,52 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('http://dev.chiragh.com/')
+
+WebUI.maximizeWindow()
+
+WebUI.click(findTestObject('Page_Chiragh/div_Login or Register'))
+
+WebUI.click(findTestObject('Object Repository/upload profile picture/Page_Chiragh/a_Login'))
+
+WebUI.setText(findTestObject('Object Repository/upload profile picture/Page_Chiragh/input_Sign In_userName'), 'newuser01')
+
+WebUI.setEncryptedText(findTestObject('Object Repository/upload profile picture/Page_Chiragh/input_Sign In_userPassword'),
+	'RigbBhfdqOBDK95asqKeHw==')
+
+WebUI.click(findTestObject('Object Repository/upload profile picture/Page_Chiragh/button_Sign In'))
+
+WebUI.delay(4)
+
+WebUI.click(findTestObject('Page_Chiragh/div_Login or Register'))
+
+WebUI.click(findTestObject('Object Repository/upload profile picture/Page_Chiragh/a_Profile'))
+
+WebUI.verifyElementPresent(findTestObject('upload profile picture/Page_Chiragh/div_Personal InfoPersonal Deta'), 5)
+
+WebUI.uploadFile(findTestObject('upload profile picture/Page_Chiragh/div_Personal InfoPersonal Deta'), 'C:\\Users\\HP\\Katalon Studio\\register\\Include\\images\\800px_COLOURBOX5609690.jpg')
+
+WebUI.verifyElementPresent(findTestObject('Object Repository/upload profile picture/Page_Chiragh/snack-bar-container_Profile im'),
+	5)
+
+WebUI.verifyElementPresent(findTestObject('Object Repository/upload profile picture/Page_Chiragh/span_Profile image uploaded su'),
+	5)
+
+WebUI.closeBrowser()
+
+
